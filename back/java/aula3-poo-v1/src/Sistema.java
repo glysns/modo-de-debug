dee0f3f59cf5e7d621eed56feb9c9dd4ccdc1eef
Carlos Henrique <carlos.nascto1604@gmail.com>
2023-10-18 13:58:04 -0300

    add objects

--- back/java/aula3-poo-v1/src/Sistema.java
+++ back/java/aula3-poo-v1/src/Sistema.java
@@ -1,9 +1,33 @@
-import cadastros.Cadastro;
+import cadastros.*;
+import correntista.Agencia;
 import correntista.Conta;
+
+import java.time.LocalDate;
 
 public class Sistema {
     public static void main(String[] args) {
         Cadastro gleyson = new Cadastro();
+        gleyson.setNome("GLEYSON SAMPAIO");
+        gleyson.setCpf("618.677.980-77");
+        gleyson.setDataNascimento(LocalDate.of(1990, 7, 8));
+
+        Endereco endereco = new Endereco();
+        endereco.setBairro("Angelim");
+        endereco.setCep("64041-700");
+        endereco.setLogradouro("Rua Pranchita");
+        endereco.setNumero("386");
+
+        Cidade cidade = new Cidade();
+        cidade.setNome("Teresina");
+
         Conta contaCorrente = new Conta();
+        contaCorrente.setNumero("76509-");
+
+        Agencia agencia = new Agencia();
+        agencia.setNumero("3345-0");
+
+        Sexo.MASCULINO.toString();
+        Uf.MA.toString();
+        EstadoCivil.CASADO.toString();
     }
 }
