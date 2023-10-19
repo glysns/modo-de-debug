be2e4621ba14560502a7c2d737b1572e7bdc26c0
Carlos Henrique <carlos.nascto1604@gmail.com>
2023-10-18 22:27:43 -0300

    tentando converter objeto string em csv: erro

--- back/java/aula3-poo-v1/src/Sistema.java
+++ back/java/aula3-poo-v1/src/Sistema.java
@@ -2,7 +2,14 @@ import cadastros.*;
 import correntista.Agencia;
 import correntista.Conta;
 
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Sistema {
     public static void main(String[] args) {
@@ -11,23 +18,57 @@ public class Sistema {
         gleyson.setCpf("618.677.980-77");
         gleyson.setDataNascimento(LocalDate.of(1990, 7, 8));
 
-        Endereco endereco = new Endereco();
-        endereco.setBairro("Angelim");
-        endereco.setCep("64041-700");
-        endereco.setLogradouro("Rua Pranchita");
-        endereco.setNumero("386");
+        gleyson.getEndereco().setBairro("Angelim");
+        gleyson.getEndereco().setCep("64041-700");
+        gleyson.getEndereco().setLogradouro("Rua Pranchita");
+        gleyson.getEndereco().setNumero("386");
 
-        Cidade cidade = new Cidade();
-        cidade.setNome("Teresina");
+        gleyson.getCidade().setNome("Teresina");
 
-        Conta contaCorrente = new Conta();
-        contaCorrente.setNumero("76509-");
+        gleyson.getConta().setNumero("76509-");
 
-        Agencia agencia = new Agencia();
-        agencia.setNumero("3345-0");
+        gleyson.getAgencia().setNumero("3345-0");
 
-        Sexo.MASCULINO.toString();
-        Uf.PI.toString();
-        EstadoCivil.CASADO.toString();
+        gleyson.setSexo(Sexo.MASCULINO);
+        gleyson.setUf(Uf.PI);
+        gleyson.setEstadoCivil(EstadoCivil.CASADO);
+
+        List<Cadastro> cadastros = new ArrayList<>();
+        cadastros.add(gleyson);
+
+        escreverLayoutDelimitado(cadastros);
+    }
+
+    public static void escreverLayoutDelimitado(List<Cadastro> cadastros) {
+        System.out.println("***** - LAYOUT DELIMITADO - *****");
+
+        try {
+            StringBuilder conteudo = new StringBuilder();
+
+            for (Cadastro cadastro : cadastros) {
+                conteudo.append(cadastro.getNome()).append(";");
+                conteudo.append(cadastro.getCpf()).append(";");
+                conteudo.append(cadastro.getDataNascimento()).append(";");
+                conteudo.append(cadastro.getEndereco().getBairro()).append(";");
+                conteudo.append(cadastro.getEndereco().getCep()).append(";");
+                conteudo.append(cadastro.getEndereco().getLogradouro()).append(";");
+                conteudo.append(cadastro.getEndereco().getNumero()).append(";");
+                conteudo.append(cadastro.getCidade().getNome()).append(";");
+                conteudo.append(cadastro.getConta().getNumero()).append(";");
+                conteudo.append(cadastro.getAgencia().getNumero()).append(";");
+                conteudo.append(cadastro.getSexo()).append(";");
+                conteudo.append(cadastro.getUf()).append(";");
+                conteudo.append(cadastro.getEstadoCivil()).append(";");
+                conteudo.append(System.lineSeparator());
+            }
+            System.out.println(conteudo.toString());
+
+            Path arquivoDestino = Paths.get("C:\\arquivo-csv\\cadastros-modelo-delimitado.csv");
+
+            Files.write(arquivoDestino, conteudo.toString().getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
--- back/java/aula3-poo-v1/src/cadastros/Cadastro.java
+++ back/java/aula3-poo-v1/src/cadastros/Cadastro.java
@@ -1,5 +1,8 @@
 package cadastros;
 
+import correntista.Agencia;
+import correntista.Conta;
+
 import java.time.LocalDate;
 import java.util.Date;
 
@@ -7,6 +10,69 @@ public class Cadastro {
     private String nome;
     private LocalDate dataNascimento;
     private String cpf;
+    private Endereco endereco;
+    private Cidade cidade;
+    private Conta conta;
+    private Agencia agencia;
+    private Sexo sexo;
+    private Uf uf;
+    private EstadoCivil estadoCivil;
+
+    public Cidade getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
+
+    public Conta getConta() {
+        return conta;
+    }
+
+    public void setConta(Conta conta) {
+        this.conta = conta;
+    }
+
+    public Agencia getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(Agencia agencia) {
+        this.agencia = agencia;
+    }
+
+    public Sexo getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Sexo sexo) {
+        this.sexo = sexo;
+    }
+
+    public Uf getUf() {
+        return uf;
+    }
+
+    public void setUf(Uf uf) {
+        this.uf = uf;
+    }
+
+    public EstadoCivil getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(EstadoCivil estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
 
     public String getNome() {
         return nome;
